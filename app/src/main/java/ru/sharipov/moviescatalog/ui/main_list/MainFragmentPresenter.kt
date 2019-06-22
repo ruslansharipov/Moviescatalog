12b7fd0fb517c4412023956bbdaf434f791475f3
ruslansharipov <ruslan.b.sharipov@gmail.com>
2019-06-22 09:35:11 +0300

    Убрал дублирование кода в презентере

--- app/src/main/java/ru/sharipov/moviescatalog/ui/main_list/MainFragmentPresenter.kt
+++ app/src/main/java/ru/sharipov/moviescatalog/ui/main_list/MainFragmentPresenter.kt
@@ -1,7 +1,9 @@
 package ru.sharipov.moviescatalog.ui.main_list
 
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.functions.BiConsumer
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers
@@ -32,10 +34,49 @@ class MainFragmentPresenter(
         fetchMovieList()
     }
 
+    fun onFavouriteClick(id: Int, isChecked: Boolean) = when (isChecked) {
+        true -> favesRepository.saveId(id)
+        false -> favesRepository.removeId(id)
+    }
+
+    fun onRefresh(query: String) {
+        viewState.hideSwipeRefresh()
+        onTextChanged(query)
+    }
+
+    fun onTextChanged(query: String) {
+        if (query.isNotEmpty()) {
+            fetchFilteredMovies(query)
+        } else {
+            fetchMovieList()
+        }
+    }
+
+    private val movieListSubscriber: BiConsumer<List<MovieItem>, Throwable> = BiConsumer { movies, error ->
+        if (error != null) {
+            viewState.hideListProgress()
+            viewState.hideSearchProgress()
+        } else {
+            viewState.hideListProgress()
+            viewState.hideSearchProgress()
+            viewState.showMovies(movies)
+        }
+    }
+
     private fun fetchMovieList() {
         viewState.showListProgress()
-        compositeDisposable += moviesRepository.getMovies()
-            .flattenAsObservable { it }
+        val movies = moviesRepository.getMovies()
+        compositeDisposable += mapAndSubscribe(movies)
+    }
+
+    private fun fetchFilteredMovies(query: String) {
+        viewState.showSearchProgress()
+        val searchResult = moviesRepository.getSearchResult(query)
+        compositeDisposable += mapAndSubscribe(searchResult)
+    }
+
+    private fun mapAndSubscribe(single: Single<List<Movie>>): Disposable {
+        return single.flattenAsObservable { it }
             .map { movie: Movie -> mapMovieToItem(movie) }
             .toList()
             .subscribeOn(Schedulers.io())
@@ -56,48 +97,5 @@ class MainFragmentPresenter(
             releaseDate,
             isFavourite
         )
-    }
-
-    private fun onSuccess(movies: List<MovieItem>) {
-        viewState.hideListProgress()
-        viewState.hideSearchProgress()
-        viewState.showMovies(movies)
-    }
-
-    private fun onError(t: Throwable) {
-        viewState.hideListProgress()
-        viewState.hideSearchProgress()
-    }
-
-    fun onFavouriteClick(id: Int, isChecked: Boolean) = when (isChecked) {
-        true -> favesRepository.saveId(id)
-        false -> favesRepository.removeId(id)
-    }
-
-    fun onRefresh(searchInput: String) {
-        viewState.hideSwipeRefresh()
-        if (searchInput.isNotEmpty()) {
-            onTextChanged(searchInput)
-        } else {
-            fetchMovieList()
-        }
-    }
-
-    fun onTextChanged(query: String) {
-        viewState.showSearchProgress()
-        compositeDisposable += moviesRepository.getSearchResult(query)
-            .flattenAsObservable { it }
-            .map { movie: Movie -> mapMovieToItem(movie) }
-            .toList()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(movieListSubscriber)
-    }
-
-    private val movieListSubscriber: BiConsumer<List<MovieItem>, Throwable> = BiConsumer { movies, error ->
-        when {
-            error != null -> onError(error)
-            else -> onSuccess(movies)
-        }
     }
 }
